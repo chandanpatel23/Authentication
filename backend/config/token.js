d6c494791c7952e65d3104cd344eb0880afa6bcb
Chandan Patel <chandanpatel23@gmail.com>
2026-07-26 21:05:39 +0530

    logout after page refresh problem solved

--- backend/config/token.js
+++ backend/config/token.js
@@ -1,8 +1,10 @@
 import jwt from "jsonwebtoken"
 
 const generateToken = (id) => {
-    let token = jwt.sign({id},process.env.JWT_SECRET,
-        {expiresIn:"7d"})
-        return token
+    let token = jwt.sign({ userId: id }, process.env.JWT_SECRET, {
+        expiresIn: "7d"
+    })
+    return token
 }
+
 export default generateToken
